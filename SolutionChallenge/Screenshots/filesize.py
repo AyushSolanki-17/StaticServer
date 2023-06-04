215eec587ce99c9a35f3296824e2807bbd490655
Ayush <ayush17solanki@gmail.com>
2023-06-04 21:28:47 +0530

    Added New Images

--- SolutionChallenge/Screenshots/filesize.py
+++
@@ -1,8 +0,0 @@
-import os
-size = os.path.getsize("D:\StaticServer\SolutionChallenge\Screenshots\demofile.txt")
-print("Size before Alteration: ",size)
-with open('demofile.txt', 'ab') as f:
-    f.write(b'\0' * 1000)
-
-size = os.path.getsize("D:\StaticServer\SolutionChallenge\Screenshots\demofile.txt")
-print("Size after Alteration (1000 Bytes): ",size)
